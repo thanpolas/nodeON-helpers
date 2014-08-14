29b69c36e3098a086a949ba0add904439549f979
Thanasis Polychronakis <thanpolas@gmail.com>
2014-08-14 21:54:59 +0300

    BOOM

---
+++ test/spec/helper.test.js
@@ -0,0 +1,12 @@
+/**
+ * @fileOverview Base API Surface tests.
+ */
+var Promise = require('bluebird');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Base API Surface', function() {
+  it('should do what...', function(){
+     // test!
+  });
+});
